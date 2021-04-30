fcda0fad6528dd73f65e0206ae1b65f48ddbf308
Jasmine-Attal <81757828+Jasmine-Attal@users.noreply.github.com>
2021-04-30 15:47:22 +0530

    Add files via upload

---
+++ Log.js
@@ -0,0 +1,12 @@
+class Log extends BaseClass {
+  constructor(x,y,height,angle){
+   super(x,y,20,height,angle)
+      Matter.Body.setAngle(this.body, angle);
+     this.image=loadImage("wood2.png") 
+    }
+    display(){
+      
+     super.display();
+    }
+  }
+  
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# c25-inheritence
+# angryBirdsStage2StudentActivity
+Student Activity Link for Angry Birds Stage 2
